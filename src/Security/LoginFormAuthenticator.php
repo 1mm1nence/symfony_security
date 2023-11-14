f92eefaf9e323a68ac55565977de0c8f79d6da5e
1mm1nence <110335004+1mm1nence@users.noreply.github.com>
2023-11-14 11:19:26 +0200

    authentication proccess written, on...Success and on...Failure tried

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -3,10 +3,12 @@
 namespace App\Security;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
@@ -14,6 +16,12 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 class LoginFormAuthenticator extends AbstractAuthenticator
 {
+    public function __construct(
+        private UserRepository $userRepository
+    )
+    {
+    }
+
     public function supports(Request $request): ?bool
     {
         // TODO: Implement supports() method.
@@ -28,9 +36,17 @@ class LoginFormAuthenticator extends AbstractAuthenticator
         $password = $request->request->get('password');
 
         return new Passport(
-            new UserBadge($email),
+        new UserBadge($email, function($userIdentifier) {
+            $user = $this->userRepository->findOneBy(['email' => $userIdentifier]);
+
+            if (!$user) {
+                throw new UserNotFoundException();
+            }   
+
+            return $user;
+        }),
             new CustomCredentials(function($credentials, User $user) {
-                dd($credentials, $user);
+                return $credentials === 'tada';
             }, $password)
         );
     }
@@ -38,11 +54,13 @@ class LoginFormAuthenticator extends AbstractAuthenticator
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
         // TODO: Implement onAuthenticationSuccess() method.
+        dd('success');
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
         // TODO: Implement onAuthenticationFailure() method.
+        dd('failure');
     }
 
 //    public function start(Request $request, AuthenticationException $authException = null): Response
